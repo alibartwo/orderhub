cc79fb8567d74f3b61332b44480ba989c5710c9f
alibartwo <alibartwo>
2024-09-15 16:03:15 +0000

    added create-contact root for contact create form page

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import ContactsView from '../views/ContactsView.vue';
 import OrdersView from '../views/OrdersView.vue';
+import ContactCreateView from '../views/ContactCreateView.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
   { path: '/contacts', name: 'Contacts', component: ContactsView },
   { path: '/orders', name: 'Orders', component: OrdersView },
+  { path: '/create-contact', name: 'Create Contact', component: ContactCreateView },
 ];
 
 const router = createRouter({
