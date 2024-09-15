d45ac605eec9f5b124f7121a3ebf9078fd7f5d5b
alibartwo <alibartwo>
2024-09-15 15:28:46 +0000

    added pinia for store management and axios for api requests

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import './assets/styles.scss';
 import router from './routers';
 import AppLayout from './layout/AppLayout.vue';
 
+const pinia = createPinia();
 const app = createApp(AppLayout);
+
+app.use(pinia);
 app.use(router);
 app.mount('#app');
