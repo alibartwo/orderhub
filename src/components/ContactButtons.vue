96e5b0bfbde0bad264434dfc3178f21528195d2c
alibartwo <alibartwo>
2024-09-15 16:29:01 +0000

    added toggleble search input functionality

--- src/components/ContactButtons.vue
+++ src/components/ContactButtons.vue
@@ -3,19 +3,27 @@
     <button @click="loadSampleContacts">Load Sample Contacts</button>
     <button @click="showCreateForm">Add New Contact</button>
     <button @click="confirmDeleteAllContacts">Delete All Contacts</button>
-    <button @click="showSearchForm">Search a Contact</button>
+    <button @click="toggleSearch">Search a Contact</button>
+
+    <div v-if="showSearchInput" class="search-form">
+      <input v-model="searchTerm" type="text" placeholder="Enter contact name..." />
+      <button @click="searchContact">Search</button>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { useContactStore } from '../stores/useContactStore';
 import { sampleContacts } from '../data/sampleContacts';
+import { ref } from 'vue';
 import router from '../routers';
 const contactStore = useContactStore();
 const { removeAllContacts, loadSampleData, contacts } = contactStore;
 
 const loadSampleContacts = () => {
-  const confirmation = confirm('Are you sure you want to load sample contacts? This action will be delete existing contacts and create the sample ones');
+  const confirmation = confirm(
+    'Are you sure you want to load sample contacts? This action will be delete existing contacts and create the sample ones'
+  );
   if (confirmation) {
     loadSampleData();
   }
@@ -31,7 +39,20 @@ const confirmDeleteAllContacts = () => {
   }
 };
 
-const showSearchForm = () => {};
+const searchTerm = ref('');
+const showSearchInput = ref(false);
+
+const toggleSearch = () => {
+  showSearchInput.value = !showSearchInput.value;
+};
+
+const searchContact = () => {
+  if (searchTerm.value) {
+    // contactStore.searchContacts(searchTerm.value);
+  } else {
+    alert('Please enter a search term.');
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
