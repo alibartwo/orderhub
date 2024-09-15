b3ac89e37a5ab3704c537033e896e0cec1601b45
alibartwo <alibartwo>
2024-09-15 16:08:15 +0000

    created a contact for adding new contacts

---
+++ src/views/ContactCreateView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="contact-create">
+    <h1>Create a New Contact</h1>
+    <Card> <ContactForm /> </Card>
+  </div>
+</template>
+
+<script setup>
+import Card from '../components/Card.vue';
+import ContactForm from '../components/ContactForm.vue';
+</script>
+
+<style land="scss" scoped></style>
