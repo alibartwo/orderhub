0a78bdab20cae1dd6ceb7befe72df8ee34dd140e
alibartwo <alibartwo>
2024-09-16 22:56:40 +0000

    created order editing page to display order form

---
+++ src/views/OrderEditView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Edit an Order</h1>
+    <Card>
+      <GoBack />
+      <OrderForm />
+    </Card>
+  </div>
+</template>
+<script setup>
+import Card from '../components/Card.vue';
+import OrderForm from '../components/OrderForm.vue';
+import GoBack from '../components/GoBack.vue';
+</script>
+<style lang="scss" scoped></style>
