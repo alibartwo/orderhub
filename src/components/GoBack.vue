fbb1ece45b16fe7d8698e7d988b5684d8c6ffdf9
alibartwo <alibartwo>
2024-09-16 22:45:39 +0000

    created go back button component for common usage

---
+++ src/components/GoBack.vue
@@ -0,0 +1,11 @@
+<template>
+  <button class="go-back-button" @click="goBack">⬅</button>
+</template>
+
+<script lang="ts" setup>
+const goBack = () => {
+  window.history.back();
+};
+</script>
+
+<style scoped lang="scss"></style>
