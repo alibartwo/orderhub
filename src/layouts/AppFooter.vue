95a4a3a4613cb1c85b58f197d3022e7e9ce3033b
alibartwo <alibartwo>
2024-09-16 22:51:37 +0000

    renamed the folder where the layout files in

---
+++ src/layouts/AppFooter.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+    <div class="layout-footer">
+        <span class="footer"><i>ascatu front-end challange</i></span>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
