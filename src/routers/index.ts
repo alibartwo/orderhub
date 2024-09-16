d90be47ac611d85ab88ad46d40c29ff0bccd527c
alibartwo <alibartwo>
2024-09-16 22:52:03 +0000

    added routes for order views

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -4,6 +4,9 @@ import ContactsView from '../views/ContactsView.vue';
 import OrdersView from '../views/OrdersView.vue';
 import ContactCreateView from '../views/ContactCreateView.vue';
 import ContactEditView from '../views/ContactEditView.vue';
+import OrderCreateView from '../views/OrderCreateView.vue';
+import OrderEditView from '../views/OrderEditView.vue';
+import OrderDetailsView from '../views/OrderDetailsView.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
@@ -11,6 +14,9 @@ const routes = [
   { path: '/orders', name: 'Orders', component: OrdersView },
   { path: '/create-contact', name: 'Create Contact', component: ContactCreateView },
   { path: '/edit-contact/:id', name: 'Edit Contact', component: ContactEditView },
+  { path: '/create-order', name: 'Create Order', component: OrderCreateView },
+  { path: '/edit-order/:id', name: 'Edit Order', component: OrderEditView },
+  { path: '/order-details/:id', name: 'Order Details', component: OrderDetailsView },
 ];
 
 const router = createRouter({
