dccc4ca270c0a0698fa88b5d0e22d73178661321
alibartwo <alibartwo>
2024-09-16 08:04:34 +0000

    added route for edit contact page

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,12 +3,14 @@ import HomeView from '../views/HomeView.vue';
 import ContactsView from '../views/ContactsView.vue';
 import OrdersView from '../views/OrdersView.vue';
 import ContactCreateView from '../views/ContactCreateView.vue';
+import ContactEditView from '../views/ContactEditView.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
   { path: '/contacts', name: 'Contacts', component: ContactsView },
   { path: '/orders', name: 'Orders', component: OrdersView },
   { path: '/create-contact', name: 'Create Contact', component: ContactCreateView },
+  { path: '/edit-contact/:id', name: 'Edit Contact', component: ContactEditView },
 ];
 
 const router = createRouter({
