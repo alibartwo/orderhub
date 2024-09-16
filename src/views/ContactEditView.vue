139ccba0c928584db6e3f569ea54a494cbe31e52
alibartwo <alibartwo>
2024-09-16 08:09:44 +0000

    created contact edit view to display contact edit form

---
+++ src/views/ContactEditView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="contact-edit">
+      <h1>Edit the Contact</h1>
+      <Card>
+        <button @click="goBack">＜</button>
+        <ContactEditForm />
+      </Card>
+    </div>
+  </template>
+  
+  <script setup>
+  import { useRouter } from 'vue-router';
+  import Card from '../components/Card.vue';
+  import ContactEditForm from '../components/ContactEditForm.vue';
+  
+  const router = useRouter();
+  
+  const goBack = () => {
+    router.back();
+  };
+  </script>
+  
+  <style land="scss" scoped></style>
+  
