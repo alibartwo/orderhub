70349464c72af5d8e741ee5d98a242c8963b0a4d
alibartwo <alibartwo>
2024-09-16 08:08:55 +0000

    added go back button

--- src/views/ContactCreateView.vue
+++ src/views/ContactCreateView.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="contact-create">
     <h1>Create a New Contact</h1>
-    <Card> <ContactForm /> </Card>
+    <Card>
+      <button @click="goBack">＜</button>
+      <ContactCreateForm />
+    </Card>
   </div>
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router';
 import Card from '../components/Card.vue';
-import ContactForm from '../components/ContactForm.vue';
+import ContactCreateForm from '../components/ContactCreateForm.vue';
+
+const router = useRouter();
+
+const goBack = () => {
+  router.back();
+};
 </script>
 
 <style land="scss" scoped></style>
