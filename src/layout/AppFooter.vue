84c264df34e89c126c1734ab1d41dfc2a1f22f70
alibartwo <alibartwo>
2024-09-16 22:57:28 +0000

    renamed or deleted

--- src/layout/AppFooter.vue
+++
@@ -1,9 +0,0 @@
-<script setup></script>
-
-<template>
-    <div class="layout-footer">
-        <span class="footer"><i>ascatu front-end challange</i></span>
-    </div>
-</template>
-
-<style lang="scss" scoped></style>
