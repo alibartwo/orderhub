ffc4ef5086f09a4d38bd60270a44796c6683495c
alibartwo <alibartwo>
2024-09-16 22:55:27 +0000

    add the basic view of home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,12 @@
-<script setup></script>
-
 <template>
-    This is home page
+  <div>
+    <h1>Home Page</h1>
+    <Card><h3>This application made for the frontend challange</h3></Card>
+  </div>
 </template>
+
+<script setup>
+import Card from '../components/Card.vue';
+</script>
 
 <style lang="scss" scoped></style>
