12aa608532ca8e412d2d7fa701e83bf34f5345af
alibartwo <alibartwo>
2024-09-16 22:54:17 +0000

    used go back component instead of using directly

--- src/views/ContactCreateView.vue
+++ src/views/ContactCreateView.vue
@@ -1,23 +1,17 @@
 <template>
   <div class="contact-create">
     <h1>Create a New Contact</h1>
-    <Card>
-      <button @click="goBack">＜</button>
+    <Card
+      ><GoBack />
       <ContactCreateForm />
     </Card>
   </div>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
 import Card from '../components/Card.vue';
 import ContactCreateForm from '../components/ContactCreateForm.vue';
-
-const router = useRouter();
-
-const goBack = () => {
-  router.back();
-};
+import GoBack from '../components/GoBack.vue';
 </script>
 
 <style land="scss" scoped></style>
--- src/views/ContactEditView.vue
+++ src/views/ContactEditView.vue
@@ -2,22 +2,16 @@
     <div class="contact-edit">
       <h1>Edit the Contact</h1>
       <Card>
-        <button @click="goBack">＜</button>
+        <GoBack />
         <ContactEditForm />
       </Card>
     </div>
   </template>
   
   <script setup>
-  import { useRouter } from 'vue-router';
   import Card from '../components/Card.vue';
   import ContactEditForm from '../components/ContactEditForm.vue';
-  
-  const router = useRouter();
-  
-  const goBack = () => {
-    router.back();
-  };
+  import GoBack from '../components/GoBack.vue'
   </script>
   
   <style land="scss" scoped></style>
