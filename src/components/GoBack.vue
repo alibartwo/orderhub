bef777da5c636b076e39315797dce3ce79b86599
alibartwo <alibartwo>
2024-09-18 07:17:05 +0000

    assigned button type and replaced windows.history with router.back

--- src/components/GoBack.vue
+++ src/components/GoBack.vue
@@ -1,11 +1,15 @@
+<!-- this is reusable button component with going back functionality -->
 <template>
-  <button class="go-back-button" @click="goBack">⬅</button>
+  <button type="button" class="go-back-button" @click="goBack">⬅</button>
 </template>
 
 <script lang="ts" setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
 const goBack = () => {
-  window.history.back();
+  router.back();
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style lang="scss"></style>
