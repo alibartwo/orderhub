65f9dbb407389f2b4908c80b5f60514f290238cc
alibartwo <alibartwo>
2024-09-18 07:26:30 +0000

    added alert if en error occures to

--- src/components/OrderDetails.vue
+++ src/components/OrderDetails.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="order-details" v-if="order">
+    <!-- this edit button will be in the top right corner of the table for editing the displayed order -->
     <button class="edit-button" @click="editOrder(orderID)">✏️</button>
+    <!-- order details will be displayed in the form of a table with the headings on the side -->
     <table>
       <tbody>
         <tr>
@@ -13,15 +15,21 @@
         </tr>
         <tr>
           <th>Sold To:</th>
-          <td>{{ order.soldTo.firstName }} {{ order.soldTo.lastName }}, Address: {{ formattedAddress(order.soldTo) }}</td>
+          <td>
+            {{ order.soldTo.firstName }} {{ order.soldTo.lastName }}, Address: {{ formattedAddress(order.soldTo) }}
+          </td>
         </tr>
         <tr>
           <th>Bill To:</th>
-          <td>{{ order.billTo.firstName }} {{ order.billTo.lastName }}, Address: {{ formattedAddress(order.billTo) }}</td>
+          <td>
+            {{ order.billTo.firstName }} {{ order.billTo.lastName }}, Address: {{ formattedAddress(order.billTo) }}
+          </td>
         </tr>
         <tr>
           <th>Ship To:</th>
-          <td>{{ order.shipTo.firstName }} {{ order.shipTo.lastName }}, Address: {{ formattedAddress(order.shipTo) }}</td>
+          <td>
+            {{ order.shipTo.firstName }} {{ order.shipTo.lastName }}, Address: {{ formattedAddress(order.shipTo) }}
+          </td>
         </tr>
         <tr>
           <th>Order Value:</th>
@@ -35,6 +43,7 @@
           <th>Currency:</th>
           <td>{{ order.currencyCode }}</td>
         </tr>
+        <!-- separate row for each order item -->
         <tr v-for="(item, index) in order.items" :key="index">
           <th>Item {{ index + 1 }}:</th>
           <td>Product ID: {{ item.productID }}, Quantity: {{ item.quantity }}, Item Price: {{ item.itemPrice }}</td>
@@ -46,8 +55,8 @@
 
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue';
-import { useOrderStore } from '../stores/useOrderStore';
 import { useRoute, useRouter } from 'vue-router';
+import { useOrderStore } from '../stores/useOrderStore';
 import type { Order } from '../services/orderService';
 import { useFormatters } from '../composables/useFormatters';
 
@@ -55,15 +64,16 @@ const { formattedAddress } = useFormatters();
 const router = useRouter();
 const route = useRoute();
 const orderStore = useOrderStore();
-const orderID = route.params.id as string;
 
+const orderID = route.params.id as string;
 const order = ref<Order>();
 
 const loadOrder = async () => {
   try {
     order.value = await orderStore.getOrder(orderID);
   } catch (err) {
-    console.error('Error loading order details:', err);
+    console.error('Error occured while loading order details:', err);
+    alert('Error occured while loading order details:');
   }
 };
 
@@ -71,6 +81,7 @@ const editOrder = (id: string) => {
   router.push(`/edit-order/${id}`);
 };
 
+// load the order when the component is mounted
 onMounted(loadOrder);
 </script>
 
