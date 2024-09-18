904ddd0bf299eb139a861a0a3145db5fa3ee5a57
alibartwo <alibartwo>
2024-09-18 07:29:33 +0000

    refactored function to use Person instead of any

--- src/composables/useFormatters.ts
+++ src/composables/useFormatters.ts
@@ -1,5 +1,8 @@
+// this composable provides utility functions for formatting data within the application.
+import { Person } from "../services/contactService";
+
 export const useFormatters = () => {
-  const formattedAddress = (contact: any) => {
+  const formattedAddress = (contact: Person): string => {
     return `${contact.country}, ${contact.city}, ${contact.streetAddress}, ${contact.houseNumber}, zip: ${contact.zip}`;
   };
 
