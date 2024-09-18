223a96393e3b78d7bca449ddca0fae87487a5618
alibartwo <alibartwo>
2024-09-18 07:27:36 +0000

    added comments

--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -2,6 +2,7 @@
   <div class="list">
     <p v-if="loading">Loading...</p>
     <p v-if="error">{{ error }}</p>
+    <!-- order list shown only with not loading and no errors -->
     <table v-if="!loading && !error">
       <thead>
         <tr>
@@ -13,6 +14,7 @@
         </tr>
       </thead>
       <tbody>
+        <!-- loop through each order and display them in a row -->
         <tr v-for="order in orders" :key="order.orderID">
           <td>{{ order.orderID }}</td>
           <td>{{ order.orderDate }}</td>
@@ -35,14 +37,18 @@ import { useOrderStore } from '../stores/useOrderStore';
 
 const router = useRouter();
 const orderStore = useOrderStore();
+
+// computed properties for reactive state management
 const orders = computed(() => orderStore.orders);
 const loading = computed(() => orderStore.loading);
 const error = computed(() => orderStore.error);
 
+// function to navigate to edit order page
 const editOrder = (id: string) => {
   router.push(`/edit-order/${id}`);
 };
 
+// function to delete an order after confirmation
 const deleteOrder = (id: string) => {
   const confirmation = confirm(`Are you sure you want to delete the contact: ${id}? This action cannot be undone.`);
   if (confirmation) {
@@ -50,12 +56,15 @@ const deleteOrder = (id: string) => {
   }
 };
 
+// function to navigate to order detail page
 const orderDetail = (id: string) => {
   router.push(`order-details/${id}`);
 };
 
+// load the orders when the component is mounted
 onMounted(() => {
   orderStore.loadOrders();
 });
+
 </script>
 <style lang="scss" scoped></style>
